89e161c0258a5bf08b9ce8640fb967b6dd066de7
Elmer177 <192658953+Elmer177@users.noreply.github.com>
2026-04-19 22:00:47 +0900

    fix:scroll

--- asset/js/scroll.js
+++ asset/js/scroll.js
@@ -1,7 +1,18 @@
-const symbol = document.querySelector('.scroll-symbol');
+// ページロード時にスクロール禁止
+function noscroll(e){
+    e.preventDefault();
+}
+document.addEventListener('touchmove', noscroll, {passive: false});
+document.addEventListener('wheel', noscroll, {passive: false});
 
+// スクロールシンボルをクリックしたときの処理
+const symbol = document.querySelector('.scroll-symbol');
 symbol.addEventListener('click', (e) => {
   e.preventDefault();
+  // スクロール禁止を解除
+  document.removeEventListener('touchmove', noscroll, {passive: false});
+  document.removeEventListener('wheel', noscroll, {passive: false});
+  
   const scrollamount = window.innerHeight; // 画面の高さ分スクロール
   window.scrollBy({
     top: scrollamount,
