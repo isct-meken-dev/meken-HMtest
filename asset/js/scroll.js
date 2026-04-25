f5c6ca754ee6a61ba9fe0b0f2fbda1a8d1c92c33
Elmer177 <192658953+Elmer177@users.noreply.github.com>
2026-04-25 17:00:06 +0900

    fix:scroll

--- asset/js/scroll.js
+++ asset/js/scroll.js
@@ -1,22 +1,32 @@
-// ページロード時かつトップにいる間だけスクロールを禁止する
-function noscroll(e) {
-  if (window.scrollY === 0) {
-    e.preventDefault();
-  }
-}
-
-document.addEventListener('touchmove', noscroll, { passive: false });
-document.addEventListener('wheel', noscroll, { passive: false });
-
 // スクロールシンボルをクリックしたときの処理
 const symbol = document.querySelector('.scroll-symbol');
+
 if (symbol) {
   symbol.addEventListener('click', (e) => {
     e.preventDefault();
-    const scrollamount = window.innerHeight; // 画面の高さ分スクロール
+    const curY_bottom = window.scrollY + window.innerHeight; // 現在のスクロール位置の下端
+    const scrollamount = 2 * window.innerHeight - curY_bottom; // 画面下端が2*window.innerHeightになるように
     window.scrollBy({
       top: scrollamount,
       behavior: 'smooth'
     });
   });
 }
+
+
+// スクロール時に一度だけ条件を満たしたら自動スクロール
+(function firstscroll() {
+  let alreadyScrolled = false;
+  window.addEventListener('scroll', () => {
+    if (alreadyScrolled) return;
+    const curY_bottom = window.scrollY + window.innerHeight;
+    if (curY_bottom >= window.innerHeight && curY_bottom < 2 * window.innerHeight) {
+      // 画面下端が2*window.innerHeightになるように絶対位置でスクロール
+      window.scrollTo({
+        top: 2 * window.innerHeight - window.innerHeight,
+        behavior: 'smooth'
+      });
+      alreadyScrolled = true;
+    }
+  });
+})();
